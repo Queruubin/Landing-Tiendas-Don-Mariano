8111c422596f97b9d1f289dd74c858ffa2ec3123
Samuel Castelblanco <samuelcf0123@gmail.com>
2026-01-25 22:45:03 -0500

    Reviews section added and SEO imporved

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.tiendasdonmariano.com', // Reemplaza con tu dominio real cuando lo tengas
   vite: {
     plugins: [tailwindcss()],
   }
